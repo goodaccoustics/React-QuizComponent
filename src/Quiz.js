1b22820dfef77a93e718f322dffdd199193805df
goodaccoustics <goodaccoustics@hotmail.com>
2018-11-01 14:35:02 +0000

    commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,12 +5,14 @@ let quizData = require('./quiz_data.json');
 class Quiz extends Component {
 	constructor(props) {
 		super(props);
-
+		this.state = {
+			quiz_position: 1
+		}
 	}
 	render() {
 		return (
 			<div>
-				Hello World
+				Quiz
 			</div>
 		);
 	}
