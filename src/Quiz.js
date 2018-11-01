b81a031274dab30c4b0c4fed26b82158b913db46
goodaccoustics <goodaccoustics@hotmail.com>
2018-11-01 14:27:35 +0000

    adding quiz.js

---
+++ src/Quiz.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+	render() {
+		return (
+			<div>
+				Hello World
+			</div>
+		);
+	}
+}
+
+export default Quiz;
