0700c292f26101200add4ea3565969e784bc35d3
goodaccoustics <goodaccoustics@hotmail.com>
2018-11-02 12:37:38 +0000

    commit

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,11 +5,13 @@ class QuizQuestion extends Component {
 		return (
 			<main>
 				<section>
-					<p>//instruction text goes here</p>
+					<p>
+						{this.props.quiz_question.instruction_text}
+					</p>
 				</section>
 				<section className='buttons'>
 					<ul>
-						//quiz question button logic goes here
+						{this.props.quiz_question.answer_options[0]}
 					</ul>
 				</section>
 			</main>
